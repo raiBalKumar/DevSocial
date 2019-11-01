c3f6e3f36b1dd68fd770a9b23d0f396f4ab49b92
Bala-office <preitam_rai@yahoo.com>
2019-11-01 12:07:06 +0800

    friday

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const config = require("config");
+const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
@@ -59,9 +61,23 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-      // Return jsonwebtoken
 
-      res.send("User Registered");
+      // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.log(err.message);
       res.status(500).json("Server error");
